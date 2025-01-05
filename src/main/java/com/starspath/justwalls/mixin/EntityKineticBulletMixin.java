4bbd7442959b2a179e27c8ac5fcd7e0cb16d5944
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-01-05 16:02:05 +0100

    finaly works

--- src/main/java/com/starspath/justwalls/mixin/EntityKineticBulletMixin.java
+++ src/main/java/com/starspath/justwalls/mixin/EntityKineticBulletMixin.java
@@ -4,34 +4,32 @@ import com.starspath.justwalls.blocks.abstracts.StructureBlock;
 import com.starspath.justwalls.world.DamageBlockSaveData;
 import com.tacz.guns.entity.EntityKineticBullet;
 import com.tacz.guns.resource.pojo.data.gun.BulletData;
-import com.tacz.guns.resource.pojo.data.gun.GunData;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityKineticBullet.class)
 public abstract class EntityKineticBulletMixin {
-
     @Shadow private int blockDamage;
 
-    @Inject(method = "<init>(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;ZLcom/tacz/guns/resource/pojo/data/gun/GunData;Lcom/tacz/guns/resource/pojo/data/gun/BulletData;)V",
-            at = @At("RETURN"))
-    private void captureBlockDamage(Level worldIn, LivingEntity throwerIn, ItemStack gunItem, ResourceLocation ammoId, ResourceLocation gunId, boolean isTracerAmmo, GunData gunData, BulletData bulletData, CallbackInfo ci) {
-        this.blockDamage = bulletData.getBlockDamage();
+    @ModifyVariable(
+            method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;ZLcom/tacz/guns/resource/pojo/data/gun/GunData;Lcom/tacz/guns/resource/pojo/data/gun/BulletData;)V",
+            at = @At("HEAD"),
+            argsOnly = true
+    )
+    private static BulletData modifyBulletData(BulletData bulletData) {
+        return bulletData;
     }
 
-    @Inject(method = "onHitBlock", at = @At("HEAD"), cancellable = true,remap = false)
+    @Inject(method = "onHitBlock", at = @At("HEAD"), cancellable = true, remap = false)
     private void onHitStructureBlock(BlockHitResult result, Vec3 startVec, Vec3 endVec, CallbackInfo ci) {
         EntityKineticBullet bullet = (EntityKineticBullet)(Object)this;
         Level level = bullet.level();
@@ -41,7 +39,6 @@ public abstract class EntityKineticBulletMixin {
         if(blockState.getBlock() instanceof StructureBlock structureBlock) {
             BlockPos masterPos = structureBlock.getMasterPos(level, pos, blockState);
             if(masterPos != null) {
-                BlockState masterState = level.getBlockState(masterPos);
                 DamageBlockSaveData damageBlockSaveData = DamageBlockSaveData.get(level);
 
                 if(damageBlockSaveData.damageBlock(level, masterPos, this.blockDamage) <= 0) {
